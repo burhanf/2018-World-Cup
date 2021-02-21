51a69e4a108aef9379e5f6b1b21f41303d97292b
Burhan Faquiri <faquiri@sheridancollege.ca>
2021-02-21 17:19:11 -0500

    Added more teams that will be loaded into the database when app is launched and a placeholder text for adding a new team's name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
 INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('France', 'Europe', 7, 6, 1, 0);
 INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Belgium', 'Europe', 7, 6, 0, 1);
+INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Croatia', 'Europe', 7, 4, 2, 1);
 INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Uruguay', 'S. America', 5, 4, 0, 1);
 INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Brazil', 'S. America', 5, 3, 1, 1);
+INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('England', 'Europe', 7, 3, 1, 3);
+INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Sweden', 'Europe', 5, 3, 0, 2);
 INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Russia', 'Europe', 5, 2, 2, 1);
+INSERT INTO Teams (TeamName, Continent, Played, Won, Drawn, Lost) VALUES ('Canada', 'N. America', 5, 2, 2, 1);
